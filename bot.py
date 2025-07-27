80de3a1700efcf5ef53f3c6bc59b55674aaa6627
xEffect-code <vova99.kal4enko@gmail.com>
2025-07-27 18:16:57 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -4,14 +4,17 @@ from config import BOT_TOKEN
 from handlers import router as main_router
 from support_handlers import router as support_router
 from admin_handlers import router as admin_router
+from aiogram.fsm.storage.memory import MemoryStorage
 
 bot = Bot(token=BOT_TOKEN)
 
 async def main():
     dp = Dispatcher()
+    storage = MemoryStorage()
+    dp = Dispatcher(storage=storage)
+    dp.include_router(admin_router)
     dp.include_router(main_router)
     dp.include_router(support_router)
-    dp.include_router(admin_router)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
